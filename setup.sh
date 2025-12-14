c20d7c7572005c7cc7e2e5d02efc85403cf9b366
Z313861 <a026191@qq.com>
2025-12-14 22:27:39 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip install streamlit==1.28.0
+pip install scikit-learn==1.3.0
+pip install matplotlib==3.7.0
+pip install pandas==2.0.0
+pip install numpy==1.24.0
+pip install plotly==5.17.0
